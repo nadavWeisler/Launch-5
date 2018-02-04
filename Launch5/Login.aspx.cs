29d72b1fe3c29f84281e24ee9017e5005788999a
nadav weisler <weisler.nadav@gmail.com>
2018-02-04 19:00:39 +0200

    init project

---
+++ Launch5/Login.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Launch5
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if(Label1.Text == "barak")
+            {
+                Label1.Text = "nadav";
+            }
+            else
+            {
+                Label1.Text = "barak";
+            }
+        }
+    }
+}
