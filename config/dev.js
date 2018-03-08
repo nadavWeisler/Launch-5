733a51c65eef8980e17e0413921b5cf795d86408
nadav weisler <weisler.nadav@gmail.com>
2018-03-08 07:40:49 +0200

    heroku deployment

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,8 @@
 //dev.js dont commit it
 module.exports = {
-    googleClientID: '308594530688-bojvlpn3q064vseqb29hcsh4bsua53ma.apps.googleusercontent.com',
-    googleClientSecret: 'bNMWuE6w_yRdGepaYWsdz61L',
+    googleClientID: '841663533825-p8f1np6v7cutnp1clm4ikbhg701nv3on.apps.googleusercontent.com',
+    googleClientSecret: 'nm_R6rkUIRCfcPOIrykFNqhE',
     MONGODB_URI: "mongodb://Nadav:NadavLaunch5@ds151908.mlab.com:51908/launch5_dev",
-    JWT_SECRET: "herzog",
     PORT: "3001",
     cookieKey: "herzog",
     stripePublishableKey: "pk_test_YqLj4g4wmSHBzdGJ7xrX8Dxj",
